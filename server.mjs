ab16ccd0dcc583d47afc07f77c495fe5635d0c89
Alexey Ermakov <strangebrew@yandex.ru>
2022-01-06 14:46:48 +0300

    Add event emitter, remove timers

--- server.mjs
+++ server.mjs
@@ -1,48 +1,56 @@
-import { createServer } from "http";
+import { EventEmitter } from "events";
 import { createReadStream } from "fs";
+import { createServer } from "http";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-let data;
+class SSE extends EventEmitter {
+	constructor() {
+		super();
+	}
 
-function sse(_req, res) {
-	res.setHeader("Content-Type", "text/event-stream");
-	res.setHeader("Cache-Control", "no-cache");
-	res.setHeader("Connection", "keep-alive");
+	init(_req, res) {
+		res.setHeader("Content-Type", "text/event-stream");
+		res.setHeader("Cache-Control", "no-cache");
+		res.setHeader("Connection", "keep-alive");
 
-	let id = 0;
+		this.res = res;
 
-	const timer = setInterval(() => {
-		if (data !== undefined && data !== "") {
-			res.write(`data: ${data}\n`);
+		let id = 0;
+
+		const dataListener = (data) => {
+			if (data.event) {
+				res.write(`event: ${data.event}\n`);
+			}
+			res.write(`data: ${data.data}\n`);
 			res.write(`id: ${++id}\n`);
 			res.write("\n");
 		}
-	}, 1000);
 
-	setTimeout(() => {
-		clearInterval(timer);
-		res.write("event: end-of-stream\n");
-		res.write("data: this is the end\n");
-		res.write("\n");
-		res.end("Ok");
-	}, 10000);
+		this.on("data", dataListener);
+	}
+
+	send(data) {
+		this.emit("data", data);
+	}
 }
+
+const sse = new SSE();
 
 createServer((req, res) => {
 	const url = new URL(`http://${req.headers.host}${req.url}`);
 
 	if (url.pathname === "/stream") {
-		sse(req, res);
+		sse.init(req, res);
 		return;
 	}
 
 	if (url.pathname === "/send-message") {
 		data = url.searchParams.get("message");
-		res.end(data);
+		sse.send({ data });
 		return;
 	}
 
